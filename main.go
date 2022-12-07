54b217bc1428b0aadef42f21ef535ea368c24e21
Tamal Saha <tamal@appscode.com>
2022-12-08 01:19:13 +0600

    added-all
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- main.go
+++ main.go
@@ -1,4 +1,27 @@
 package main
 
+import (
+	"encoding/json"
+	"io"
+	"os"
+
+	"github.com/meilisearch/meilisearch-go"
+)
+
 func main() {
+	client := meilisearch.NewClient(meilisearch.ClientConfig{
+		Host: "http://localhost:7700",
+	})
+
+	jsonFile, _ := os.Open("movies.json")
+	defer jsonFile.Close()
+
+	byteValue, _ := io.ReadAll(jsonFile)
+	var movies []map[string]interface{}
+	json.Unmarshal(byteValue, &movies)
+
+	_, err := client.Index("movies").AddDocuments(movies)
+	if err != nil {
+		panic(err)
+	}
 }
